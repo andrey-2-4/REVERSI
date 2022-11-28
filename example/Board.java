154de7d8a24ca36617814a086b4ae583736f22d6
Andrey <61345502+andrey-2-4@users.noreply.github.com>
2022-11-28 21:53:36 +0300

    Add files via upload

--- example/Board.java
+++ example/Board.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  * Класс, содержащий логику игры
  */
 public class Board {
+    private static final Scanner SCANNER = new Scanner(System.in);
     private int[][] board;
     private boolean[][] boardOfPossibleMoves;
     private int bestScorePlayer1 = -1;
@@ -113,21 +117,172 @@ public class Board {
      */
     private boolean updateBoardOfPossibleMoves(int playerNumber) {
         boardOfPossibleMoves = new boolean[8][8];
-        // проходим по элементам          for
-        // проверяем, что он 0,
+        boolean gameIsPossible = false;
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 0 && getIndexesOfClampedCells(playerNumber, i, j).size() != 0) {
+                    gameIsPossible = true;
+                    boardOfPossibleMoves[i][j] = true;
+                }
+            }
+        }
+        return gameIsPossible;
+    }
 
-        // что рядом есть противоположное число, (вокруг)
-
-        // (в направлении противоположного числа) мы что-то замыкаем
-        // СКОРЕЕ ВСЕГО ПРИДЕТСЯ СДЕЛАТЬ ОТДЕЛЬНУЮ ФУНКЦИЮ ДЛЯ НАХОЖДЕНИЯ ЭЛЕМЕНТОВ, КОТОРЫЕ МЫ ЗАЖИМАЕМ
-        // + К ЭТОМУ боард оф элементов, которые меняют цвет и В ФУНКЦИИ МЕНЯТЬ ЭТО ХЕРНЮ
-
-        // мы в этом методе короче меняем boardOfPossibleMoves
-        // TODO
-        // если было хотя бы 1 изменение вернем true иначе false
-
-        
-        return true;
+    private Vector<Integer> getIndexesOfClampedCells(int playerNumber, int x, int y) {
+        Vector<Integer> indexesOfClampedCells = new Vector<>();
+        Vector<Integer> tempIndexes;
+        int i, j;
+        boolean closed;
+        // налево наверх
+        i = x - 1;
+        j = y - 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(0 <= i && 0 <= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            --i;
+            --j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // направо наверх
+        i = x - 1;
+        j = y + 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(0 <= i && 7 >= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            --i;
+            ++j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // направо вниз
+        i = x + 1;
+        j = y + 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(7 >= i && 7 >= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            ++i;
+            ++j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // налево вниз
+        i = x + 1;
+        j = y - 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(7 >= i && 0 <= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            ++i;
+            --j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // направо
+        i = x;
+        j = y + 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(7 >= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            ++j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // вниз
+        i = x + 1;
+        j = y;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(7 >= i && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            ++i;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // налево
+        i = x;
+        j = y - 1;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(0 <= j && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            --j;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        // наверх
+        i = x - 1;
+        j = y;
+        closed = false;
+        tempIndexes = new Vector<>();
+        while(0 <= i && board[i][j] != 0) {
+            if (board[i][j] == playerNumber) {
+                closed = true;
+                break;
+            } else {
+                tempIndexes.add(i);
+                tempIndexes.add(j);
+            }
+            --i;
+        }
+        if (closed) {
+            indexesOfClampedCells.addAll(tempIndexes);
+        }
+        return indexesOfClampedCells;
     }
 
     private void makeMoveByPlayer(int playerNumber) {
@@ -135,8 +290,28 @@ public class Board {
             displayBoard(true);
             displayPossibleMoves();
             System.out.println("Ход Игрока " + playerNumber + " [введите координаты вашего хода в формате 'x y']");
-            // TODO взять допустимый ход
-            // Изменить в соответствии ему доску
+            int x = -2, y = -2;
+            while (true) {
+                try {
+                    String string = SCANNER.nextLine();
+                    String[] strings = string.split(" ");
+                    x = Integer.parseInt(strings[0]);
+                    y = Integer.parseInt(strings[1]);
+                } catch (Exception exception) {
+                    System.out.println("Некорректный ввод!");
+                    continue;
+                }
+                if (x < 8 && x >= 0 && y < 8 && y >= 0 && boardOfPossibleMoves[x][y]) {
+                    break;
+                } else {
+                    System.out.println("Невозможный ход!");
+                }
+            }
+            var indexesToChangeColor = getIndexesOfClampedCells(playerNumber, x, y);
+            board[x][y] = playerNumber;
+            for (int i = 0; i < indexesToChangeColor.size(); i = i + 2) {
+                board[indexesToChangeColor.get(i)][indexesToChangeColor.get(i + 1)] = playerNumber;
+            }
         } else {
             System.out.println("Игрок " + playerNumber + " не может ходить, ход передается");
         }
